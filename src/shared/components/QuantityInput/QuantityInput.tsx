2cde5abc6262d3bbb81c0935a959e7d0381a875a
hritvikkaushik <hritvikkaushik@gmail.com>
2021-06-16 18:14:34 +0530

    Update QuantityInput to interact with state.cart

--- src/shared/components/QuantityInput/QuantityInput.tsx
+++ src/shared/components/QuantityInput/QuantityInput.tsx
@@ -1,23 +1,59 @@
 import {
-    Box, NumberDecrementStepper,
-    NumberIncrementStepper,
-    NumberInput,
-    NumberInputField,
-    NumberInputStepper
+  Box,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
 } from "@chakra-ui/react";
 import React from "react";
+import { useAppDispatch, useAppSelector } from "../../../app/hooks";
+import {
+  increaseQuantity,
+  decreaseQuantity,
+} from "../../../app/state/slices/cart.slice";
 import "./QuantityInput.css";
 
-type QuantityInputProps = {};
+type QuantityInputProps = {
+  id: number;
+};
 
 export const QuantityInput = (props: QuantityInputProps) => {
+  const qty = useAppSelector((state) => {
+    for (let i = 0; i < state.cart.items.length; ++i) {
+      if (state.cart.items[i].id === props.id) return state.cart.items[i].qty;
+    }
+  });
+
+  const dispatch = useAppDispatch();
+
   return (
     <Box display="flex">
-      <NumberInput display="flex" size="md" maxW="68px" placeholder="0" min={1} max={10}>
+      <NumberInput
+        display="flex"
+        size="sm"
+        maxW="68px"
+        min={1}
+        max={10}
+        value={qty}
+        contentEditable={false}
+      >
         <NumberInputField />
         <NumberInputStepper>
-          <NumberIncrementStepper>+</NumberIncrementStepper>
-          <NumberDecrementStepper>-</NumberDecrementStepper>
+          <NumberIncrementStepper
+            onClick={() => {
+              dispatch(increaseQuantity(props.id));
+            }}
+          >
+            +
+          </NumberIncrementStepper>
+          <NumberDecrementStepper
+            onClick={() => {
+              dispatch(decreaseQuantity(props.id));
+            }}
+          >
+            -
+          </NumberDecrementStepper>
         </NumberInputStepper>
       </NumberInput>
     </Box>
