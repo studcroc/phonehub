fc95836ec6e60ce3cddf9ff7d9bc9ad3de01b0b7
Harsh Vardhan Gautam <hvgautam24.vnit@gmail.com>
2021-06-16 14:41:56 +0530

    *Update ProductDetailsRoute Component *Add QuantityInput Component

--- src/routes/details/components/ProductDetailsRoute/ProductDetailsRoute.tsx
+++ src/routes/details/components/ProductDetailsRoute/ProductDetailsRoute.tsx
@@ -1,12 +1,63 @@
-import React from 'react';
-import './ProductDetailsRoute.css';
+import { Box, Button, Heading, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useAppSelector } from "../../../../app/hooks";
+import "./ProductDetailsRoute.css";
 
-type ProductDetailsRouteProps = {
-
-}
+type ProductDetailsRouteProps = {};
 
 export const ProductDetailsRoute = (props: ProductDetailsRouteProps) => {
-    return (
-        <h1>This is ProductDetailsRoute</h1>
-    );
-}
+  let { id }: any = useParams();
+  const productList = useAppSelector((state) => state.product.productsList);
+  const selectedProduct = productList.find(
+    (value) => value.id.toString() === id
+  );
+
+  return (
+    <Box
+      marginTop={["56px", "106px"]}
+      paddingX={["auto", "24px"]}
+      display="flex"
+      flexDirection={["column", "row"]}
+    >
+      <Box
+        flex="1"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Image
+          alt={`${selectedProduct?.model}`}
+          src={`${selectedProduct?.image}`}
+          width="100%"
+        />
+      </Box>
+      <Box
+        flex="2"
+        paddingY={["24px", "72px"]}
+        paddingX="24px"
+        justifyContent="space-between"
+        display="flex"
+        flexDirection="column"
+      >
+        <Box>
+          <Heading as="h1">{`${selectedProduct?.model}`}</Heading>
+          <Heading
+            as="h2"
+            fontSize="x-large"
+            fontWeight="light"
+            marginBottom="24px"
+          >{`${selectedProduct?.brand} `}</Heading>
+          <Text textAlign={["justify", "start"]} maxW={["full", "70%"]}>{`${selectedProduct?.description}`}</Text>
+        </Box>
+        <Box display="flex" alignItems="center" marginTop={["24px", "auto"]}>
+          <Heading
+            as="h3"
+            fontSize="xxx-large"
+          >{`$${selectedProduct?.price}`}</Heading>
+            <Button colorScheme="blue" marginLeft="24px">Add to Cart</Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
--- src/shared/components/Navbar/Navbar.tsx
+++ src/shared/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Box, Container, Icon, Image } from "@chakra-ui/react";
 import React from "react";
 import { IoCartOutline } from "react-icons/io5";
+import { Link, useLocation } from "react-router-dom";
 import Logo from "../../../assets/logo.png";
 import { ToolBar } from "../../../routes/home/components/ToolBar/ToolBar";
 import "./Navbar.css";
@@ -8,6 +9,8 @@ import "./Navbar.css";
 type NavbarProps = {};
 
 export const Navbar = (props: NavbarProps) => {
+
+  let location = useLocation();
       
   return (
     <Container zIndex="1"
@@ -28,14 +31,13 @@ export const Navbar = (props: NavbarProps) => {
       justifyContent="space-between"
       maxWidth="100%"
     >
-      {/* <Icon as={BiHomeSmile} fontSize="xx-large" cursor="pointer" color="white" /> */}
-      <Image src={Logo} height="40px" width="40px"/>
+      <Link to="/"><Image src={Logo} height="40px" width="40px" cursor="pointer" /></Link>
       <Box position="relative">
         <Box position="absolute" right="-4px" top="-4px" backgroundColor="#FFE500" borderRadius="50%" height="18px" width="18px" display="flex" alignItems="center" justifyContent="center" padding="10px">1</Box>
         <Icon as={IoCartOutline} fontSize="xx-large" cursor="pointer" color="white" />
       </Box>
     </Container>
-    <ToolBar />
+    {location.pathname === "/"?<ToolBar />: null}
     </Container>
   );
 };
