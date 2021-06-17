f0000156b164e855862d5b3849616efeb3e94c1c
Harsh Vardhan Gautam <hvgautam24.vnit@gmail.com>
2021-06-17 10:31:58 +0530

    Add button states for 'Add To Cart' button to handle different cases

--- src/routes/details/components/ProductDetailsInfo/ProductDetailsInfo.tsx
+++ src/routes/details/components/ProductDetailsInfo/ProductDetailsInfo.tsx
@@ -1,52 +1,82 @@
-import { Box, Button, Heading, Text } from '@chakra-ui/react';
-import React from 'react';
-import { useAppDispatch } from '../../../../app/hooks';
-import { addToCart } from '../../../../app/state/slices/cart.slice';
-import { ProductData } from '../../../../app/types';
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import React, { MouseEventHandler } from "react";
+import { Link } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../../../app/hooks";
+import { addToCart } from "../../../../app/state/slices/cart.slice";
+import { ProductData } from "../../../../app/types";
 
 type ProductDetailsInfoProps = {
-    selectedProduct: ProductData | undefined
-}
+  selectedProduct: ProductData | undefined;
+};
 
 export const ProductDetailsInfo = (props: ProductDetailsInfoProps) => {
+  const dispatch = useAppDispatch();
+  const cart = useAppSelector((state) => state.cart);
 
-    const dispatch = useAppDispatch();
+  const isProductAlreadyPresentInCart: Function = (): boolean => {
+    let cartItems = cart.items;
+    let isAlreadyPresentInCart = false;
+    cartItems.forEach((cartItem) => {
+      if (cartItem.id === props.selectedProduct?.id) {
+        isAlreadyPresentInCart = true;
+        return;
+      }
+    });
+    return isAlreadyPresentInCart;
+  };
 
-    return (
-        <Box
-        flex="2"
-        paddingY={["24px", "72px"]}
-        paddingX="24px"
-        justifyContent="space-between"
-        display="flex"
-        flexDirection="column"
-      >
-        <Box>
-          <Heading as="h1">{`${props.selectedProduct?.model}`}</Heading>
-          <Heading
-            as="h2"
-            fontSize="x-large"
-            fontWeight="light"
-            marginBottom="24px"
-          >{`${props.selectedProduct?.brand} `}</Heading>
-          <Text
-            textAlign={["justify", "start"]}
-            maxW={["full", "70%"]}
-          >{`${props.selectedProduct?.description}`}</Text>
-        </Box>
-        <Box display="flex" alignItems="center" marginTop={["24px", "auto"]}>
-          <Heading
-            as="h3"
-            fontSize="xxx-large"
-          >{`$${props.selectedProduct?.price}`}</Heading>
+  const getButtonText: Function = (): string => {
+    if (isProductAlreadyPresentInCart()) {
+      return "Go To Cart";
+    } else {
+      return "Add To Cart";
+    }
+  };
+
+  const handleAddToCart: MouseEventHandler<HTMLElement> = (): void => {
+    if (isProductAlreadyPresentInCart()) {
+    } else {
+      dispatch(addToCart(props.selectedProduct as ProductData));
+    }
+  };
+
+  return (
+    <Box
+      flex="2"
+      paddingY={["24px", "72px"]}
+      paddingX="24px"
+      justifyContent="space-between"
+      display="flex"
+      flexDirection="column"
+    >
+      <Box>
+        <Heading as="h1">{`${props.selectedProduct?.model}`}</Heading>
+        <Heading
+          as="h2"
+          fontSize="x-large"
+          fontWeight="light"
+          marginBottom="24px"
+        >{`${props.selectedProduct?.brand} `}</Heading>
+        <Text
+          textAlign={["justify", "start"]}
+          maxW={["full", "70%"]}
+        >{`${props.selectedProduct?.description}`}</Text>
+      </Box>
+      <Box display="flex" alignItems="center" marginTop={["24px", "auto"]}>
+        <Heading
+          as="h3"
+          fontSize="xxx-large"
+        >{`$${props.selectedProduct?.price}`}</Heading>
+        <Link to={isProductAlreadyPresentInCart()? "/cart": "#"}>
           <Button
             colorScheme="blue"
             marginLeft="24px"
-            onClick={() => dispatch(addToCart(props.selectedProduct as ProductData))}
+            onClick={handleAddToCart}
           >
-            Add to Cart
+            {getButtonText()}
           </Button>
-        </Box>
+        </Link>
       </Box>
-    );
-}
+    </Box>
+  );
+};
